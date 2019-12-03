a089850fca0389b5e581c8986296d2a17186cbf2
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-12-03 11:44:30 +0100

    tsc

--- dist/events/event.controller.js
+++ dist/events/event.controller.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const event_model_1 = __importDefault(require("./event.model"));
+const mockData = require('./event-mock-100.json');
 class EventsController {
     constructor() {
         this.path = '/events';
@@ -16,10 +17,23 @@ class EventsController {
                 res.send(events);
             });
         };
+        this.getAllEventsLocal = (req, res) => {
+            res.send(mockData);
+        };
+        this.modifyEvent = (req, res) => {
+            const id = req.params.id;
+            const eventData = req.body;
+            this.event.findByIdAndUpdate(id, eventData, { new: true })
+                .then(event => {
+                res.send(event);
+            });
+        };
         this.initializeRoutes();
     }
     initializeRoutes() {
         this.router.get(this.path, this.getAllEvents);
+        this.router.get(`${this.path}Local`, this.getAllEventsLocal);
+        this.router.patch(`${this.path}/:id`, this.modifyEvent);
     }
 }
 exports.default = EventsController;
